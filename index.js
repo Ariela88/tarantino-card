c621c103912ea261719a00d89a355b2aced64918
Ariela88 <135212709+Ariela88@users.noreply.github.com>
2023-07-08 07:28:44 +0200

    mediaquery add

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const film1 = new Film('./assets/pf-card.webp','Pulp Fiction',1994,'9 su 10')
-const film2 = new Film('./assets/bastard.jpg','Bastardi Senza Gloria',2009,'8.5 su 10')
+const film2 = new Film('./assets/bastard.jpg','Bastardi Senza Gloria',2009,'8 su 10')
 const film3 = new Film('./assets/kill.jpg','Kill Bill Vol.1',2003,'9 su 10')
-const film4 = new Film('./assets/iene.jpg','Le Iene',1992,'9.5 su 10')
+const film4 = new Film('./assets/iene.jpg','Le Iene',1992,'9 su 10')
 const film5 = new Film('./assets/jackie.jpg','Jackie Brown',1997,'8 su 10')
 const film6 = new Film('./assets/four.jpg','Four Rooms',1995,'7 su 10')
-const film7 = new Film('./assets/django.jpg','Django Unchained',2012,'7.5 su 10')
+const film7 = new Film('./assets/django.jpg','Django Unchained',2012,'7,5 su 10')
 const film8 = new Film('./assets/hateful.jpg','The Hateful Eight',2015,'7 su 10')
-const film9 = new Film('./assets/kill2.jpg','Kill bil Vol.2',204,' 7.5 su 10')
+const film9 = new Film('./assets/kill2.jpg','Kill bil Vol.2',2004,' 7 su 10')
 
 const filmArray = [film1,film2,film3,film5,film4,film6,film7,film8,film9]
 
@@ -82,16 +82,6 @@ filmArray.sort((a,b)=> a.yop - b.yop)
 }
 
 
-function sortCardByRateDown(){
-
-    
-        
-    filmArray.sort((a,b)=> a.rate.localeCompare(b.rate))
-                
-    
-    
-             renderIndex(filmArray)
-    }
 
     function sortCardByRateUp(){
 
